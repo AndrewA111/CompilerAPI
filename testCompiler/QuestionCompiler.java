05f0a16d96e878c7fb70f64c60cb68f60edecf0e
Andrew Allan <andrew.allan1@outlook.com>
2020-06-28 20:33:03 +0100

    Added up to date QuestionCompiler

--- testCompiler/QuestionCompiler.java
+++ testCompiler/QuestionCompiler.java
@@ -3,15 +3,26 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
 
+/**
+ * Class to compile and run java files for a given question and run
+ * associated tests
+ */
 public class QuestionCompiler {
+
+    /*
+     *  When compiling with javac command, each dependency is delimited by
+     *  a separator. The separator is different depending on operating system.
+     *  For unix-style systems, the separator is ':', while for windows it is ';'
+     * 
+     *  By default, separator is set to ':'
+     */
+    private static String cpSeparator = ":";
 
     /**
      * Main method used to compile question code and run TestRunner.java
      * 
-     * Single command line argument specifying source code folder location 
-     * expected
+     * Expects a single command line argument specifying source code folder location 
      * 
      * @param args
      */
@@ -20,24 +31,47 @@ public class QuestionCompiler {
             
             // arg[0] is folder location of files to be compiled
             String folderLocation = args[0];
+
+            /*
+             * To run tests, compiler must be provided with dependency locations.
+             * 
+             * Dependencies are:
+             * 
+             *      - JUnit 4.13
+             *      - Hamcrest Core 1.3
+             * 
+             * By default, these are provided in the same location as this class
+             */
             
-            // string to build file names for arguments to javac
+            // get project root dir
+            String currDir = System.getProperty("user.dir");
+
+            // set dependency locations
+            final String JUNIT_LOC = currDir + "/testCompiler/junit-4.13.jar";
+            final String HAMCREST_CORE_LOC = currDir + "/testCompiler/hamcrest-core-1.3.jar";
+
+            // if on windows OS, change cpSeparator
+            if(System.getProperty("os.name").toLowerCase().contains("windows")){
+                cpSeparator = ";";
+            }
+            
+            // String to build file names for arguments to javac
             String javaFiles = "";
 
-            // get all files in folder
+            // Get all files in folder
             File[] files = new File(folderLocation).listFiles();
 
-            // for all .java files, append to javaFiles string
+            // For all .java files, append to javaFiles string (space separated)
             for(File file: files){
                 if(file.getName().endsWith(".java")){;
                     javaFiles = javaFiles + " " + folderLocation + file.getName();
                 }
             }
 
-            // compile and run tests
-            String compOut = runCompile("javac -cp " + folderLocation + 
-            ":/home/andrew/eclipse-workspace/dropwizardTutorial/testCompiler/junit-4.13.jar " + 
-            javaFiles);
+            // Compile and run tests
+            String compOut = runCompile("javac -cp " 
+                                        + folderLocation + cpSeparator + JUNIT_LOC + " " + 
+                                        javaFiles);
 
             /*
              *  If compilation output is not empty, error has occured
@@ -45,21 +79,32 @@ public class QuestionCompiler {
              */
             if(!compOut.equals("")){
 
-                // remove file location
+                // Remove file location from error
                 compOut = compOut.replace(folderLocation, "");
+
+                // Print
                 System.out.println("Compilation error\n" + compOut);
                 return;
             }
 
-            // run the tests
-            String result = runTests("java -cp " + folderLocation + 
-            ":/home/andrew/eclipse-workspace/dropwizardTutorial/testCompiler/junit-4.13.jar:" + 
-            "/home/andrew/eclipse-workspace/dropwizardTutorial/testCompiler/hamcrest-core-1.3.jar" + " TestRunner");
+            /*
+             *  Run the tests 
+             * 
+             *  It is expected that the file TestRunner will be available, 
+             *  which runs all tests and returns results.
+             */
+            String result = runTests("java -cp " + 
+                                    folderLocation + cpSeparator + JUNIT_LOC + cpSeparator + HAMCREST_CORE_LOC + 
+                                    " TestRunner");
 
-            // empty string indicated tests passsed, print 0 to indicate
+            // Empty string indicates tests passsed, print 0 to indicate
             if(result.equals("")){
                 System.out.println("0");
             }
+            /*
+             *  Else, print result. Result with either indicate which tests have failed, 
+             *  or show any errors in running TestRunner
+             */
             else{
                 System.out.println(result);
             }
@@ -71,7 +116,7 @@ public class QuestionCompiler {
 
     /**
      * Method to run compilation
-     * @param command
+     * @param command compilation command
      */
     public static String runCompile(String command) {
           
@@ -91,7 +136,7 @@ public class QuestionCompiler {
 
     /**
      * Method to run tests
-     * @param command
+     * @param command command to run TestRunner
      */
     public static String runTests(String command) {
           
@@ -99,13 +144,13 @@ public class QuestionCompiler {
             // Execute command
             Process pro = Runtime.getRuntime().exec(command);
             
-            // string to build output
+            // String to build output
             String output = "";
 
-            // get command output
+            // Get command output
             String cOut = streamToString(pro.getInputStream());
             
-            // get error output
+            // Get error output
             String cErr = streamToString(pro.getErrorStream());
 
             /*
@@ -138,16 +183,23 @@ public class QuestionCompiler {
     /**
      * Method to read input stream and convert to a string
      * 
-     * @param is
+     * @param is input stream
      */
     public static String streamToString(InputStream is) {
 
+        // Create BufferedReader to manipulate data
         BufferedReader input = new BufferedReader(new InputStreamReader(is));
-
+        
+        // String to store each line
         String line = null;
+
+        // String to build output from lines
         String lines = "";
+
         try {
+            // Loop while new lines available
             while ((line = input.readLine()) != null) {
+                // Add line to output string
                 lines += line;
             }
             return lines;
